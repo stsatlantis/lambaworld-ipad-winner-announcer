65e5a553002c1ba066423f3c2f3758a4a8408fb0
Barnabas Olah <barnabas.olah@ext.addisongloballtd.com>
2018-10-29 09:57:48 +0100

    feat(raffle) : add implementation

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "lambaworld-ipad-winner-announcer"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
+
+libraryDependencies += "com.chuusai" %% "shapeless" % "2.3.3"
--- src/main/scala/io/barni/lambdaworld/raffle/Raffle.scala
+++ src/main/scala/io/barni/lambdaworld/raffle/Raffle.scala
@@ -1,5 +1,20 @@
 package io.barni.lambdaworld.raffle
 
-class Raffle {
+import java.math.BigInteger
+import java.security.SecureRandom
 
+import scala.io.Source
+import scala.util.Random
+object Raffle extends App {
+
+  private val random = new SecureRandom()
+  val filename = "path-to-file-name"
+  val indexSeed = new BigInteger(20, random).doubleValue().toInt
+
+  val participantsb = Source.fromFile(filename).getLines.toList
+
+  val participants = Random.shuffle(participantsb)
+  val nrOfDuplicates = participants.groupBy(identity).count{case (_,cica) => cica.size > 1}
+  val nrOfParticipants = participants.size
+  participants(indexSeed % nrOfParticipants)
 }
